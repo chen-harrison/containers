77bc0197dcbd90c56f412cc837c8ec3eb8c4b17a
Harrison Chen <hchen.robotics@gmail.com>
2025-06-09 16:50:58 -0400

    CI Pipeline v1

---
+++ build.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+check_help() {
+    if [[ $# -eq 1 && ($1 == "-h" || $1 == "--help") ]] ; then
+        return
+    fi
+    return 1
+}
+
+if [[ $# -ne 2 ]] || check_help "$@" ; then
+    echo "Usage: $(basename "$0") SOURCE_IMAGE[:TAG] OUTPUT_IMAGE[:TAG]"
+    echo "Build a dev container."
+    exit 0
+fi
+
+BASE_IMAGE=$1
+SOURCE_IMAGE=$2
+USER_UID=1000
+USER_GID=1000
+USERNAME=$(docker run --rm -u "$USER_UID" "$BASE_IMAGE" whoami 2> /dev/null) || USERNAME="user"
+
+docker build \
+    --build-arg BASE_IMAGE=$BASE_IMAGE \
+    --build-arg USERNAME=$USERNAME \
+    --build-arg USER_UID=$USER_UID \
+    --build-arg USER_GID=$USER_GID \
+    --no-cache \
+    -f "$(dirname $0)/Dockerfile" \
+    -t "${SOURCE_IMAGE}" \
+    "$(dirname $0)"
