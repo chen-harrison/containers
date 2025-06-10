01d78902ee36ec289a77e1b0cb707091f63d7619
Harrison Chen <hchen.robotics@gmail.com>
2025-06-10 16:07:04 -0400

    Add Local Build Script
    - Local build script uses native UID, GID, and username
    - Has an optional second argument - output image will be tagged as
      local:latest if not provided

---
+++ build_local.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+check_help() {
+    if [[ $# -eq 1 && ($1 == "-h" || $1 == "--help") ]] ; then
+        return
+    fi
+    return 1
+}
+
+if [[ $# -eq 0  || $# -gt 2 ]] || check_help "$@" ; then
+    echo "Usage: $(basename "$0") BASE_IMAGE[:TAG] [OUTPUT_IMAGE[:TAG]]"
+    echo "Build a dev container."
+    exit 0
+fi
+
+BASE_IMAGE=$1
+OUTPUT_IMAGE=${2:-local:latest}
+USER_UID=$(id -u)
+USER_GID=$(id -g)
+USERNAME=$(docker run --rm -u "$USER_UID" "$BASE_IMAGE" whoami 2> /dev/null) || USERNAME=$(whoami)
+
+docker build \
+    --build-arg BASE_IMAGE=$BASE_IMAGE \
+    --build-arg USERNAME=$USERNAME \
+    --build-arg USER_UID=$USER_UID \
+    --build-arg USER_GID=$USER_GID \
+    --no-cache \
+    -f "$(dirname $0)/Dockerfile" \
+    -t "${OUTPUT_IMAGE}" \
+    "$(dirname $0)"
